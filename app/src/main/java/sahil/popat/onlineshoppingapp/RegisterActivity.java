a06492a17491e35b271bb48f7d6fae1ca2a64276
SAHIL-PC <sahilpopat97@gmail.com>
2023-05-25 13:44:23 +0530

    progress

--- app/src/main/java/sahil/popat/onlineshoppingapp/RegisterActivity.java
+++ app/src/main/java/sahil/popat/onlineshoppingapp/RegisterActivity.java
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText Name, Email, Pass, ConfirmPass;
+    Button Register;
+    private String insert;
     SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         db= openOrCreateDatabase("LoginDB", Context.MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS login(name VARCHAR,email VARCHAR,password VARCHAR);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS login(name VARCHAR,email VARCHAR,password VARCHAR,confirmpass VARCHAR);");
+
+        Register=findViewById(R.id.register);
+        Register.setOnClickListener(v -> add());
+
+    }
+
+    private void add() {
+        if(insert == Register){}
     }
 }
