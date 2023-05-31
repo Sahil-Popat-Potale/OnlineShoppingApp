4abcaae5fb0b6e438459893536413129791abda3
SAHIL-PC <sahilpopat97@gmail.com>
2023-05-31 12:02:32 +0530

    progress

--- app/src/main/java/sahil/popat/onlineshoppingapp/LoginActivity.java
+++ app/src/main/java/sahil/popat/onlineshoppingapp/LoginActivity.java
@@ -1,11 +1,16 @@
 package sahil.popat.onlineshoppingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView register;
     EditText email, pass;
     Button login;
 
@@ -16,5 +21,20 @@ public class LoginActivity extends AppCompatActivity {
         email=findViewById(R.id.email);
         pass=findViewById(R.id.password);
         login=findViewById(R.id.loginBtn);
+        setText();
+
+        register=findViewById(R.id.registerNow);
+        register.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this,
+                RegisterActivity.class)));
+    }
+
+    private void setText() {
+        try {
+            String mail=email.getText().toString();
+
+
+        } catch (NullPointerException e){
+            Toast.makeText(this, "Input field is empty", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/sahil/popat/onlineshoppingapp/RegisterActivity.java
+++ app/src/main/java/sahil/popat/onlineshoppingapp/RegisterActivity.java
@@ -2,12 +2,15 @@ package sahil.popat.onlineshoppingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.database.sqlite.SQLiteDatabase;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextView oldUser;
     EditText Name, Email, Pass, ConfirmPass;
     Button Register;
     private String insert;
@@ -16,6 +19,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        oldUser=findViewById(R.id.oldUser);
+        oldUser.setOnClickListener(v -> startActivity(new Intent(RegisterActivity.this,
+                LoginActivity.class)));
+
         db= openOrCreateDatabase("LoginDB", Context.MODE_PRIVATE, null);
         db.execSQL("CREATE TABLE IF NOT EXISTS login(name VARCHAR,email VARCHAR,password VARCHAR,confirmpass VARCHAR);");
 
@@ -25,6 +32,6 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void add() {
-        if(insert == Register){}
+
     }
 }
